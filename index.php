7f03fcf2ae54270369b1c83d80318c6b12369589
Imran Sayed <imranhsayed@gmail.com>
2020-07-11 23:26:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package Aquila
+ */
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>WordPress theme</title>
+</head>
+<body>
+Hello WordPress
+</body>
+</html>

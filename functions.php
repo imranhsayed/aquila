8142981b1eb0f2d62ad146064b8143e2c990e108
Imran Sayed <imranhsayed@gmail.com>
2020-07-14 00:19:49 +0530

    Add screenshots

--- functions.php
+++ functions.php
@@ -5,16 +5,16 @@
  * @package Aquila
  */
 
-/**
- * Essential theme supports
- * */
-function theme_setup(){
+function aquila_enqueue_scripts() {
 
+	wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+	wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js' ), true );
 
-	/** tag-title **/
-	add_theme_support( 'title-tag' );
-
-
+	wp_enqueue_style( 'style-css' );
+	wp_enqueue_script( 'main-js' );
 
 }
-add_action('after_setup_theme','theme_setup');
+
+add_action( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' );
+
+?>

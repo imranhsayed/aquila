1275d644f1c2ab5cb8725902134c4a114b7aa48e
Imran Sayed <imranhsayed@gmail.com>
2020-07-13 23:30:10 +0530

    Update readme

--- functions.php
+++ functions.php
@@ -4,3 +4,17 @@
  *
  * @package Aquila
  */
+
+/**
+ * Essential theme supports
+ * */
+function theme_setup(){
+
+
+	/** tag-title **/
+	add_theme_support( 'title-tag' );
+
+
+
+}
+add_action('after_setup_theme','theme_setup');

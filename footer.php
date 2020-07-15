347230a9b9252b3fd62859843458583c7d9cc7e0
Imran Sayed <imranhsayed@gmail.com>
2020-07-15 23:37:56 +0530

    Update header

--- footer.php
+++ footer.php
@@ -6,7 +6,10 @@
  */
 ?>
 
+
 <footer>Footer</footer>
+</div>
+</div>
 
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -16,6 +16,15 @@
 </head>
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+<?php
+if ( function_exists( 'wp_body_open' ) ) {
+	wp_body_open();
+}
+?>
 
-<header>Header</header>
+<div id="page" class="site">
+	<header id="masthead" class="site-header" role="banner">
+		<?php get_template_part( 'template-parts/header/nav' ); ?>
+	</header>
+	<div id="content" class="site-content">
+

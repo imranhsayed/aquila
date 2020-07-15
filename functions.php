57be883a8cc94874cc792eab9755ee0778a27d23
Imran Sayed <imranhsayed@gmail.com>
2020-07-15 18:34:31 +0530

    Add bootrap styles and scripts'

--- functions.php
+++ functions.php
@@ -7,11 +7,21 @@
 
 function aquila_enqueue_scripts() {
 
+	// Register styles.
 	wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
-	wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js' ), true );
+	wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, 'all' );
 
+	// Register scripts.
+	wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js' ), true );
+	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', [], false, true );
+
+	// Enqueue Styles.
 	wp_enqueue_style( 'style-css' );
+	wp_enqueue_style( 'bootstrap-css' );
+
+	// Enqueue Scripts.
 	wp_enqueue_script( 'main-js' );
+	wp_enqueue_script( 'bootstrap-js' );
 
 }
 
--- header.php
+++ header.php
@@ -16,6 +16,10 @@
 </head>
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+<?php
+	if ( function_exists( 'wp_body_open' ) ) {
+		wp_body_open();
+	}
+?>
 
 <header>Header</header>

0e2395ec5738ab2a2730b1f74e2c230f8cbc476e
Imran Sayed <imranhsayed@gmail.com>
2020-07-19 10:46:42 +0530

    Add custom menus

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -5,6 +5,10 @@
  * @package Aquila
  */
 
+$menu_class = \Aquila_Theme\Inc\Menus::get_instance();
+$header_menu_id = $menu_class->get_menu_id( 'aquila-header-menu' );
+$header_menus = wp_get_nav_menu_items( $header_menu_id );
+
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -18,39 +22,60 @@
 	</button>
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Dropdown
-				</a>
-				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="#">Action</a>
-					<a class="dropdown-item" href="#">Another action</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="#">Something else here</a>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-			</li>
-		</ul>
+		<?php
+		if ( ! empty( $header_menus ) && is_array( $header_menus ) ) {
+			?>
+			<ul class="navbar-nav mr-auto">
+				<?php
+					foreach ( $header_menus as $menu_item ) {
+						if ( ! $menu_item->menu_item_parent ) {
+
+							$child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID );
+							$has_children = ! empty( $child_menu_items ) && is_array( $child_menu_items );
+							$has_sub_menu_class = ! empty( $has_children ) ? 'has-submenu' : '';
+
+							if ( ! $has_children ) {
+								?>
+								<li class="nav-item">
+									<a class="nav-link" href="<?php echo esc_url( $menu_item->url ); ?>">
+										<?php echo esc_html( $menu_item->title ); ?>
+									</a>
+								</li>
+								<?php
+							} else {
+								?>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url ); ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<?php echo esc_html( $menu_item->title ); ?>
+									</a>
+									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+										<?php
+											foreach ( $child_menu_items as $child_menu_item ) {
+												?>
+												<a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url ); ?>">
+													<?php echo esc_html( $child_menu_item->title ); ?>
+												</a>
+												<?php
+											}
+										?>
+									</div>
+								</li>
+								<?php
+							}
+							?>
+							<?php
+						}
+					}
+				?>
+			</ul>
+			<?php
+		}
+		?>
 		<form class="form-inline my-2 my-lg-0">
 			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
 			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		</form>
 	</div>
 </nav>
-<?php
-wp_nav_menu(
-	[
-		'theme_location' => 'aquila-header-menu',
-		'container_class' => 'my_extra_menu_class'
-	]
-);
+
 

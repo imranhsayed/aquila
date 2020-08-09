84a30e93a087926967ec9965a6e1edf7c3c2187f
Imran Sayed <imranhsayed@gmail.com>
2020-08-09 09:40:28 +0530

    Register sidebars sidebar 1 as Sidebar and sidebar 2 for footer and call them using dynamic sidebar function

--- footer.php
+++ footer.php
@@ -7,10 +7,17 @@
 ?>
 
 
-<footer>Footer</footer>
-</div>
-</div>
+<footer>
+	<h3>Footer</h3>
+	<?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
+		<aside>
+			<?php dynamic_sidebar( 'sidebar-2' ); ?>
+		</aside>
+	<?php } ?>
 
+</footer>
+</div>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -19,6 +19,7 @@ class AQUILA_THEME {
 		Assets::get_instance();
 		Menus::get_instance();
 		Meta_Boxes::get_instance();
+		Widgets::get_instance();
 
 		$this->setup_hooks();
 	}

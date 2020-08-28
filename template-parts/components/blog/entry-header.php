2eb4aa66198292c04de4615e910d87e02fd040cf
Imran <imranhsayed@gmail.com>
2020-08-28 23:41:56 +0530

    Update classname

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -7,7 +7,7 @@
 
 $the_post_id   = get_the_ID();
 $hide_title    = get_post_meta( $the_post_id, '_hide_page_title', true );
-$heading_class = ( ! empty( $hide_title ) && 'yes' === $hide_title ) ? 'hide' : '';
+$heading_class = ( ! empty( $hide_title ) && 'yes' === $hide_title ) ? 'hide d-none' : '';
 
 $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );
 

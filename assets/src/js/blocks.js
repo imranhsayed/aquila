6dcccf4f8f1b8c63794d9d67d01a19ae3bd33b25
Imran <imranhsayed@gmail.com>
2020-12-13 10:57:51 +0530

    Gutenberg Style variation'

--- assets/src/js/blocks.js
+++ assets/src/js/blocks.js
@@ -3,3 +3,4 @@ import '../sass/blocks.scss';
 // Blocks
 import './gutenberg/blocks/heading-with-icon';
 import './gutenberg/blocks/dos-and-donts';
+import './gutenberg/block-extensions/register-block-styles';

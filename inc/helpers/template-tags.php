c7d7e349a841b41f28c57a6087c040f3a7756a10
Imran Sayed <imranhsayed@users.noreply.github.com>
2021-06-18 16:27:08 +0530

    Update the excerpt function to use wp_html_excerpt
    Update the excerpt function to use wp_html_excerpt

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -113,11 +113,10 @@ function aquila_the_excerpt( $trim_character_count = 0 ) {
 		return;
 	}
 
-	$excerpt = wp_strip_all_tags( get_the_excerpt() );
-	$excerpt = substr( $excerpt, 0, $trim_character_count );
-	$excerpt = substr( $excerpt, 0, strrpos( $excerpt, ' ' ) );
+        $excerpt = wp_html_excerpt( get_the_excerpt( $post_ID ), $trim_character_count, '[...]' );
 
-	echo $excerpt . '[...]';
+
+	echo $excerpt;
 }
 
 /**
